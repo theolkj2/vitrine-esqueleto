3f33151c7bd96d82a356fb2a56abc0f371ffa29b
UniALFA <unialfa@MACMINI-07.local>
2023-03-01 21:25:42 -0300

    Criando o nosso index e realizando o primeiro commit

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
     $banco    = "vitrine";
 
     try {
-        $pdo = new PDO("mysql:host={$servidor};dbname={$banco};port=8889;charset=utf8;",$usuario,$senha);
+        $pdo = new PDO("mysql:host={$servidor};dbname={$banco};port=8888;charset=utf8;",$usuario,$senha);
     } catch (Exception $e) {
         echo "<p>Erro ao tentar conectar</p>";
         echo $e->getMessage();
