c888d7720995eeb1f2621ed484f9ac66920c3646
theolkj2 <matheus050319@gmail.com>
2023-03-09 19:29:06 -0300

    Arrumando o login

--- admin/paginas/login.php
+++ admin/paginas/login.php
@@ -1,40 +1,36 @@
 <?php
-        if($_POST){
-            $login = $_POST ["login"] ?? NULL;
-            $senha = $_POST ["senha"] ?? NULL;
-            
-            $sql = "select id, nome, login, senha
-            from usuario
-            where login = :login AND ativo = "S" 
-            limit 1
-            ";
-
-            $consulta = $pdo ->prepare ($sql);
-            $consulta-> bindParam (":login , $login");
-            $consulta-> execute();
-
-            $dados = $consulta->fetch(PDO: :FETCH_OBJ);
-
-            var_dump($dados);
+    if($_POST){
+        $login = $_POST['login'] ?? null;
+        $senha = $_POST['senha'] ?? null;
+        
+        if(empty($login) or empty($senha)){
+            mensagemErro("Nao pode ser vazio");
         }
-    ?>
 
-<div class="login" >
+        $sql = "SELECT id,nome,login,senha FROM usuario where login = :login and ativo = 'S' limit 1";
+        $consulta = $pdo->prepare($sql);
+        $consulta->bindParam(":login", $login);       
+        $consulta->execute();
+        $dados = $consulta->fetch(PDO::FETCH_OBJ);
+ 
+        var_dump($dados);
+        
+        //echo $login;
+    }
 
-    <h1 class="text-center">Efetuar Login</h1>
-   
-    <form method="POST" >
-        <label for="login">Login</label>
-        <input type="text" name="login" id="login" class="form-control" require placeholder="Por favor preencha este campo" >
+    
+?>
 
+
+<div class="login rounded bg-light" style="margin-top: 20vh;">
+    <h1 class="text-center ">Efetuar Login</h1>
+    <form  method="POST">
+        <label for="login" class="">Login</label>
+        <input type="text" name="login" id="login" class="form-control" required placeholder="Porfavor digite o usuario">
         <br>
-
-        <label for="senha">Senha:</label>
-        <input type="password" name="senha" id="senha" class="form-control" require placeholder="Por favor preencha este campo" >
-
+        <label for="senha">Senha</label>
+        <input type="password" name="senha" id="senha" class="form-control" required placeholder="Porfavor digite a senha">
         <br>
-
-        <button type="submit" class="btn btn-success w-100 " >Efetuar Login</button>
-
+        <button type="submit" class="btn btn-primary w-100">Login</button>
     </form>
 </div>
